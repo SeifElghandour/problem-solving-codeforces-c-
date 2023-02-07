7939649bb7bf500f655a1108078dfc5f705a0033
Seif EL-Ghandour <124669935+SeifElghandour@users.noreply.github.com>
2023-02-07 16:52:53 +0200

    Add files via upload

---
+++ 1BASSIUT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A = 3;long long B = 1234567812345; char C = 'a'; float D = 334.23; double E = 14049.30493;
+
+    cin >> A >> B >> C >> D >> E;
+    cout << A << endl;
+    cout << B << endl;
+    cout << C  << endl;
+    cout << D  << endl;
+    cout << E  << endl;
+
+        return 0;
+}
+
